02a3eea6e20088f3596e28d82bef89e01ef9924d
Seo <zenu9876@gmail.com>
2025-11-24 15:47:00 +0900

    demo v1.3 edit intro

--- components/shared/profile/my-profile.tsx
+++ components/shared/profile/my-profile.tsx
@@ -24,20 +24,25 @@ const MyProfile = ({ tech }: { tech: any }) => {
       <div className="justify-start">
         <Image src="/images/me.jpg" alt="123" width={177} height={236} />
       </div>
-      <div className="pt-6 pb-6  dark:bg-[oklch(0.208_0.042_265.755)] rounded-lg">
-        <p
-          className=" text-responsive font-medium leading-relaxed whitespace-pre-line
-        border-t-4 pt-6 sm:border-t-0 sm:border-l-4 sm:pt-0 sm:pl-6 sm:border-gray-400"
-        >
-          {`React를 중심으로 웹,모바일 등의 다양한 프로젝트와 스타트업 경험이 있는 개발자 입니다.
-책임감 있는 프론트엔드 개발자가 되기 위해 끊임없이 노력하고 있습니다.
-디지털컨텐츠 학부를 복수전공하여 디자인 감각을 가지고 사용자 경험에 맞추어 
-알맞은 UX/UI 구현을 중요하게 생각하고 있습니다.`}
-        </p>
+      <div className="pt-6 pb-6 dark:bg-[oklch(0.208_0.042_265.755)] rounded-lg">
+        <div className="space-y-6 border-t-4 pt-6 sm:border-t-0 sm:border-l-4 sm:pt-0 sm:pl-6 sm:border-gray-400">
+          {/* fade-in 애니메이션 */}
+          <p className="text-responsive font-medium leading-relaxed animate-in fade-in slide-in-from-bottom-4 duration-700">
+            웹, 모바일 등 다양한 프로젝트와 스타트업 경험이 있는 프론트엔드
+            개발자입니다.
+          </p>
+
+          <p className="text-responsive leading-loose text-muted-foreground animate-in fade-in slide-in-from-bottom-4 duration-700 delay-150 whitespace-pre-line">
+            {`실무 기간 동안 여행 플랫폼 앱 운영 및 실시간 채팅 시스템 구축, 
+외식 리뷰 플랫폼 구현 및 운영 등 다양한 프론트엔드 프로젝트를 성공적으로 완수했습니다.
+      새로운 기술을 배우고 실무에 적용하는 것을 즐기며, 팀과의 원활한
+            협업을 통해 더 나은 결과물을 만들어내는 것을 추구합니다.`}
+          </p>
+        </div>
       </div>
       <div className="flex flex-col gap-8">
         {/* 학력 */}
-        <div className="flex flex-col sm:flex-row gap-4">
+        <div className="flex flex-col sm:flex-row gap-4  ">
           <div className="space-y-4 flex-1/2">
             <h2 className="h2-responsive font-bold flex items-center gap-3">
               📚 학력
@@ -56,7 +61,7 @@ const MyProfile = ({ tech }: { tech: any }) => {
           </div>
 
           {/* 인적정보 */}
-          <div className="space-y-4 flex-1/2">
+          <div className="space-y-4 flex-1/2 ">
             <h2 className="h2-responsive font-bold flex items-center gap-3">
               🏢 인적정보
             </h2>
