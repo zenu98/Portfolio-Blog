8a12ed7dac64bcd6db3f09d1462d1875b93124b6
Seo <zenu9876@gmail.com>
2026-04-23 21:33:26 +0900

    v2.0 alpha

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
   output: "standalone",
   images: {
     remotePatterns: [
